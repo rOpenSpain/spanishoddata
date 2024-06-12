1a4cdcf13ed3653fb8aceb47497b047242244e8f
robinlovelace <rob00x@gmail.com>
2024-06-12 17:51:07 +0100

    Add get_metadata()

--- R/get.R
+++ R/get.R
@@ -22,19 +22,22 @@ get_latest_v2_xml = function(
 #' @export
 #' @examples
 #' # Get the data dictionary for the default data directory
-#' data_dict = get_data_dictionary()
-#' head(data_dict)
-get_data_dictionary = function(data_dir = get_data_dir()) {
+#' metadata = get_metadata()
+#' names(metadata)
+#' head(metadata)
+get_metadata = function(data_dir = get_data_dir()) {
   xml_files_list = fs::dir_ls(data_dir, type = "file", regexp = "data_links_") |> sort()
   latest_data_links_xml_path = utils::tail(xml_files_list, 1)
   if (length(latest_data_links_xml_path) == 0) {
     message("Getting latest data links xml")
     latest_data_links_xml_path = get_latest_v2_xml(data_dir = data_dir)
+  } else {
+    message("Using existing data links xml: ", latest_data_links_xml_path)
   }
 
   x_xml = xml2::read_xml(latest_data_links_xml_path)
 
-  download_dt = data.frame(
+  download_dt = tibble::tibble(
     target_url = xml2::xml_find_all(x = x_xml, xpath = "//link") |> xml2::xml_text(),
     pub_date = xml2::xml_find_all(x = x_xml, xpath = "//pubDate") |> xml2::xml_text()
   )
