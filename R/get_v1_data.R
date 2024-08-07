455119d1c10bbe6daf3498118b8c0bcc10d242fc
Egor Kotov <kotov.egor@gmail.com>
2024-08-07 14:27:19 +0200

    moved download function away from v1 specific file

--- R/get_v1_data.R
+++ R/get_v1_data.R
@@ -53,23 +53,29 @@ spod_get_latest_v1_file_list <- function(
 #' }
 spod_available_data_v1 <- function(data_dir = spod_get_data_dir(),
   # check_local_files (below) is FALSE by default to avoid excessive filesystem access, perhaps should be TRUE. Download functions use it to load the xml file, but we probably do not want the script to check all local cache directories every time we run a get data function. Perhaps it is better to offload this check to a separate function and have a csv file or some other way to keep track of the files that were downloaded and cached. An output of curl::multi_download() could be used for this purpose.
-  check_local_files = FALSE
+  check_local_files = FALSE,
+  quiet = FALSE
 ) {
   xml_files_list <- fs::dir_ls(data_dir, type = "file", regexp = "data_links_v1") |> sort()
-  latest_data_links_xml_path <- utils::tail(xml_files_list, 1)
+  if(length(xml_files_list) == 0) {
+    if(isFALSE(quiet)) message("No data links xml files found, getting latest data links xml")
+    latest_data_links_xml_path <- spod_get_latest_v1_file_list(data_dir = data_dir)
+  } else {
+    latest_data_links_xml_path <- utils::tail(xml_files_list, 1)
+  }
 
   # Check if the XML file is 1 day old or older from its name
   file_date <- stringr::str_extract(latest_data_links_xml_path, "[0-9]{4}-[0-9]{2}-[0-9]{2}")
 
   if (file_date < format(Sys.Date(), format = "%Y-%m-%d")) {
-    message("File list xml is 1 day old or older, getting latest data links xml")
+    if(isFALSE(quiet)) message("File list xml is 1 day old or older, getting latest data links xml")
     latest_data_links_xml_path <- spod_get_latest_v1_file_list(data_dir = data_dir)
   } else {
-    message("Using existing data links xml: ", latest_data_links_xml_path)
+    if(isFALSE(quiet)) message("Using existing data links xml: ", latest_data_links_xml_path)
   }
 
   if (length(latest_data_links_xml_path) == 0) {
-    message("Getting latest data links xml")
+    if(isFALSE(quiet)) message("Getting latest data links xml")
     latest_data_links_xml_path <- spod_get_latest_v1_file_list(data_dir = data_dir)
   }
 
@@ -198,165 +204,36 @@ spod_clean_zones_v1 <- function(zones_path) {
   return(zones)
 }
 
+
 #' Retrieve the origin-destination v1 data (2020-2021)
 #' 
-#' This function retrieves the origin-destination data from the specified data directory.
+#' This function retrieves the v1 (2020-2021) origin-destination data from the specified data directory.
 #' 
-spod_get_od_v1 <- function(
-  date_range = c("2020-02-14", "2020-02-15"),
-  dates_list = NULL,
-  date_regex = NULL,
-  zones = c("distritos", "municipios"),
+#' @inheritParams spod_download_data
+#' @return A tibble with the origin-destination data.
+spod_get_od <- function(
+  zones = c("districts", "dist", "distr",
+    "municipalities", "muni", "municip"), # add "urban_areas" for v2 data
+  dates = NULL,
   data_dir = spod_get_data_dir(),
+  quiet = FALSE,
   read_fun = duckdb::tbl_file
 ) {
-  # Processing of the date arguments is performed in subsequent functions, because we would want the downloading functions to be able to use the same date arguments for flexibility. So `spod_download_data()` will handle the date arguments.`
+  # Processing of the date arguments is performed in `spod_download_data()`
   
   zones <- match.arg(zones)
   
-  # check the locally cached and online available data
-  # and download missing files if any
-  # get local paths of the files requested with date arguments
-  metadata <- spod_download_od_v1(
-    date_range = date_range,
-    dates_list = dates_list,
-    date_regex = date_regex,
-    zones = zones
+  # use the spot_download_data() function to download any missing data
+  downloaded_files <- spod_download_data(
+    type = "od",
+    zones = zones,
+    dates = dates,
+    data_dir = data_dir
   )
+
 
   # read data from cached files
   
   
   
-}
-
-
-
-spod_download_od_v1 <- function(
-  date_range = c("2020-02-14", "2020-02-15"),
-  dates_list = NULL,
-  date_regex = NULL,
-  zones = c("districts", "dist", "distr",
-    "municipalities", "muni", "municip"),
-  data_dir = spod_get_data_dir()
-) {
-  zones <- match.arg(zones)
-  zones <- spod_zone_names_en2es(zones)
-  
-  downloaded_files <- spod_download_data(
-    date_range = date_range,
-    dates_list = dates_list,
-    date_regex = date_regex,
-    subdir = glue::glue("v1/maestra2-mitma-{zones}/ficheros-diarios/"),
-    data_dir = data_dir
-  )
-
-  return(downloaded_files)
-}
-
-#' Download the data files of specified type, zones, dates and data version
-#' 
-#' This function downloads the data files of the specified type, zones, dates and data version.
-#' @param type The type of data to download. Can be "origin-destination" (or ust "od"), or "trips_per_person" (or just "tpp") for v1 data. For v2 data "overnight_stays" (or just "os") is also available. More data types to be supported in the future. See respective codebooks for more information. [ADD CODEBOOKS!]
-#' @param zones The zones for which to download the data. Can be "districts" (or "dist", "distr") or "municipalities" (or "muni", "municip") for v1 data. Additionaly, these can be "urban_areas" (GAUs) for v2 data.
-#' @param date_range A character vector of dates, length 2, in ISO format (YYYY-MM-DD) to download the data for. Defaults to NULL.
-#' @param dates_list A character vector of dates in ISO format (YYYY-MM-DD) to download the data for. Defaults to NULL.
-#' @param date_regex A regular expression to match the dates of the data to download. Defaults to NULL.
-#' @param ver The version of the data to use. Defaults to 1. Can be 1 or 2.
-#' @param data_dir The directory where the data is stored. Defaults to the value returned by `spod_get_data_dir()` which returns the value of the environment variable `SPANISH_OD_DATA_DIR` or a temporary directory if the variable is not set.
-#' 
-#' @export 
-#' @example 
-#'\dontrun{
-#' # Download the origin-destination on district level for the a date range in March 2020
-#' spod_download_data(type = "od", zones = "districts", date_range = c("2020-03-20", "2020-03-24"), ver = 1)
-#' 
-#' # Download the origin-destination on district level for select dates in 2020 and 2021
-#' spod_download_data(type = "od", zones = "dist", dates_list = c("2020-03-20", "2020-03-24", "2021-03-20", "2021-03-24"), ver = 1)
-#' 
-#' # Download the origin-destination on municipality level using regex for a date range in March 2020 (the regex will capture the dates 2020-03-20 to 2020-03-24)
-#' spod_download_data(type = "od", zones = "municip", date_regex = "2020032[0-4]", ver = 1)
-#' }
-spod_download_data <- function(
-  type = c(
-    "od", "origin-destination",
-    "os", "overnight_stays",
-    "tpp", "trips_per_person"),
-  zones = c("districts", "dist", "distr",
-    "municipalities", "muni", "municip"), # add "urban_areas" for v2 data
-  date_range = NULL,
-  dates_list = NULL,
-  date_regex = NULL,
-  ver = 1, # infer version from dates?
-  data_dir = spod_get_data_dir()
-) {
-  # convert english data type names to spanish words used in the default data paths
-  type <- match.arg(type)
-  type <- spod_match_data_type(type = type, ver = ver)
-  
-  # convert english zone names to spanish words used in the default data paths
-  zones <- match.arg(zones)
-  zones <- spod_zone_names_en2es(zones)
-  
-
-
-  # this is where the date arguments are processed
-  # for all the wrapper functions that call this worker function
-  dates_to_use <- process_date_arguments(
-    date_range = date_range,
-    dates_list = dates_list,
-    date_regex = date_regex,
-    ver = ver
-  )
-
-  # check version
-  # replace this argument with automatic version detection based on the dates requested?
-  rlang:::check_number_whole(ver)
-  if (!ver %in% c(1, 2)) {
-    stop("Invalid version number. Must be 1 or 2.")
-  }
-  
-  # get the available  data list while checking for files already cached on disk
-  if( ver == 1) {
-    metadata <- spod_available_data_v1(data_dir = data_dir,
-      check_local_files = TRUE)
-  } else if (ver == 2) {
-    metadata <- spod_get_metadata(data_dir = data_dir)
-    # replace with spod_available_data_v2() when available, spod_get_metadata can become a wrapper with v1/v2 argument. Potentially we can even automaticaly detect the data version based on the time intervals that user requests, but this is a bit controversial, as the methodology behind v1 and v2 data generation is not the same and Nommon+MITMA do not recommend mixing those together and comparing absoloute numbers of trips.
-  }
-  
-  # match the metadata to type, zones, version and dates
-  if(ver == 1){
-    requested_files <- metadata[
-      grepl(glue::glue("v{ver}.*{type}.*{zones}"), metadata$local_path) &
-      metadata$data_ymd %in% dates_to_use,
-    ]
-  } else if(ver == 2){
-    requested_files <- metadata[
-      grepl(glue::glue("v{ver}.*{zones}.*{type}"), metadata$local_path) &
-      metadata$data_ymd %in% dates_to_use,
-    ]
-  }
-
-  files_to_download <- requested_files[!requested_files$downloaded, ]
-  
-  # pre-generate target paths for the files to download
-  fs::dir_create(
-    unique(fs::path_dir(files_to_download$local_path)),
-    recurse = TRUE)
-
-  # download the missing files
-  downloaded_files <- curl::multi_download(
-    urls = files_to_download$target_url,
-    destfiles = files_to_download$local_path,
-    progress = TRUE,
-    resume = TRUE
-  )
-
-  # set download status for downloaded files as TRUE in requested_files
-  requested_files$downloaded[requested_files$local_path %in% downloaded_files$destfile] <- TRUE
-
-  message("Retrieved data for requested dates: ", paste(dates_to_use, collapse = ", ")) # this may output too many dates, shoudl be fixed when we create a flexible date argument processing function. Keeping for now.
-
-  return(requested_files$local_path)
 }
