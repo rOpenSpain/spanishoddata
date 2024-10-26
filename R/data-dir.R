79ae90e4d0e1c907f5fcec18c58ca1b3305753de
Egor Kotov <kotov.egor@gmail.com>
2024-10-26 23:07:03 +0200

    add spod_set_data_dir function

--- R/data-dir.R
+++ R/data-dir.R
@@ -1,3 +1,51 @@
+#' Set the data directory
+#' 
+#' This function sets the data directory in the environment variable SPANISH_OD_DATA_DIR, so that all other functions in the package can access the data. It also creates the directory if it doesn't exist.
+#' 
+#' @param data_dir The data directory to set.
+#' @inheritParams global_quiet_param
+#' @return Nothing. If quiet is `FALSE`, prints a message with the path and confirmation that the path exists.
+#' @export
+#' @keywords internal
+spod_set_data_dir <- function(
+  data_dir,
+  quiet = FALSE
+){
+  data_dir_abs_path <- fs::path_abs(data_dir)
+  
+  tryCatch({
+    # Check if the directory exists; if not, attempt to create it
+    if (!fs::dir_exists(data_dir_abs_path)) {
+      if(quiet == FALSE){
+        message("Data directory ", data_dir_abs_path, " does not exist. Attempting to create it.")
+      }
+      fs::dir_create(data_dir_abs_path, recurse = TRUE)
+    }
+    data_dir_real_path <- fs::path_real(data_dir_abs_path)
+    # Check for write permissions
+    test_file <- fs::path(data_dir_real_path, ".test_write")
+    file.create(test_file)
+    fs::file_delete(test_file)
+    if(quiet == FALSE){
+      message("Data directory is writeable.")
+    }
+    
+    # Set the environment variable
+    Sys.setenv(SPANISH_OD_DATA_DIR = data_dir_real_path)
+    
+    if(quiet == FALSE){
+      message("Data directory successfully set to: ", data_dir_real_path)
+    }
+  }, error = function(e) {
+    message("Error: Unable to create or access the directory at '", data_dir_abs_path, "'.")
+    message("This may be due to write access restrictions or system permissions issues.")
+    message("Please verify that you have write permissions for the specified path and try again.")
+    stop(e) # Re-throw the error for debugging purposes, if needed
+  })
+  
+  return(invisible(TRUE))
+}
+
 #' Get the data directory
 #'
 #' This function retrieves the data directory from the environment variable SPANISH_OD_DATA_DIR.
@@ -12,7 +60,7 @@ spod_get_data_dir <- function(quiet = FALSE) {
     if (isFALSE(quiet)) warning("Warning: SPANISH_OD_DATA_DIR is not set. Using the temporary directory, which is not recommended, as the data will be deleted when the session ends.\n\n To set the data directory, use `Sys.setenv(SPANISH_OD_DATA_DIR = '/path/to/data')` or set SPANISH_OD_DATA_DIR permanently in the environment by editing the `.Renviron` file locally for current project with `usethis::edit_r_environ('project')` or `file.edit('.Renviron')` or globally for all projects with `usethis::edit_r_environ('user')` or `file.edit('~/.Renviron')`.")
     data_dir_env <- tempdir() # if not set, use the temp directory
   } else {
-    data_dir_env <- fs::path_real(data_dir_env)
+    data_dir_env <- fs::path_abs(data_dir_env)
   }
   # check if dir exists and create it if it doesn't
   if (!fs::dir_exists(data_dir_env)) {
