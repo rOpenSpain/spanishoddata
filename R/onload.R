b1b53f2d1c0e62711f7932d33ed38ba5cdf5c4b6
Egor Kotov <kotov.egor@gmail.com>
2025-01-07 16:43:06 +0100

    add onload of graphql variables

---
+++ R/onload.R
@@ -0,0 +1,11 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.spanishoddata <- list(
+    spanishoddata.graphql_api_endpoint = "https://mapas-movilidad.transportes.gob.es/api/graphql",
+    spanishoddata.user_agent = "spanishoddata R package, https://github.com/rOpenSpain/spanishoddata/"
+  )
+  toset <- !(names(op.spanishoddata) %in% names(op))
+  if (any(toset)) options(op.spanishoddata[toset])
+
+  invisible()
+}
