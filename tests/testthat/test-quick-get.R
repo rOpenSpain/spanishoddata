8b57acab5753d403f29c30706a181be7a6eb7099
Egor Kotov <kotov.egor@gmail.com>
2025-01-28 19:16:13 +0100

    remove unnecessary env code for API tests

--- tests/testthat/test-quick-get.R
+++ tests/testthat/test-quick-get.R
@@ -1,32 +1,3 @@
-# Prepare the testing environment using bundled xml files to avoid downloading data from the internet
-
-extdata_path <- system.file("extdata", package = "spanishoddata")
-gz_files <- list.files(extdata_path, pattern = "(data_links_.*\\.xml\\.gz)|(url_file_sizes_v[1-2]\\.csv\\.gz)", full.names = TRUE)
-
-if (length(gz_files) == 0) stop("No gzipped XML files found.")
-
-# Create a temporary directory
-test_data_dir <- tempfile()
-dir.create(test_data_dir, recursive = TRUE)
-# Create metadata directory
-metadata_dir <- paste0(test_data_dir, "/", spod_subfolder_metadata_cache())
-dir.create(metadata_dir, recursive = TRUE)
-
-current_date <- format(Sys.time(), format = "%Y-%m-%d", usetz = FALSE)
-
-# Copy and rename gzipped XML files to the temporary directory
-for (gz_file in gz_files) {
-  if (grepl("v1", gz_file)) {
-    file.copy(gz_file, file.path(metadata_dir, paste0("data_links_v1_", current_date, ".xml.gz")))
-  } else if (grepl("v2", gz_file)) {
-    file.copy(gz_file, file.path(metadata_dir, paste0("data_links_v2_", current_date, ".xml.gz")))
-  }
-}
-
-# Set the environment variable to the test directory
-Sys.setenv(SPANISH_OD_DATA_DIR = test_data_dir)
-# Sys.getenv("SPANISH_OD_DATA_DIR")
-
 test_that("spod_quick_get_od fails out of range dates", {
   expect_error(
     spod_quick_get_od(
@@ -91,5 +62,3 @@ test_that("spod_quick_get_od fails on invalid municipality IDs", {
     ".*Invalid municipality IDs detected.*"
   )
 })
-
-unlink(test_data_dir, recursive = TRUE)
